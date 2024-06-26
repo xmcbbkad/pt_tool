26e0cf166a830e8536dc00bc9646a551d1645965
xmcbbkad <188135446@qq.com>
2024-06-26 20:45:45 +0800

    add fluctuation feature

--- code/feature.py
+++ code/feature.py
@@ -13,3 +13,48 @@ class FeatureBuilder():
         df['boll_lower'] = df['boll_mean'] - 2 * df['boll_std']
         
         return df
+
+    def add_fluctuation(self, df):
+        df["fluctuation"] = "" 
+        last_index = 0
+        for i in range(len(df)):
+        #for i in range(5):
+            #print("-----")
+            #print(df.iloc[i]["close"])
+            for j in range(i-1, last_index-1, -1):
+                if df.iloc[i]["high"] - df.iloc[j]["low"] > 1.0:
+                    print("buy")
+                    print(df.iloc[j]["time_str"])
+                    print(df.iloc[j]["low"])
+                    print("sell")
+                    print(df.iloc[i]["time_str"])
+                    print(df.iloc[i]["high"])
+                    print("----")
+                    #df.iloc[j]["fluctuation"] = "buy"
+                    #df.iloc[i]["fluctuation"] = "sell"
+                    df.loc[j, "fluctuation"] = "left_buy"
+                    df.loc[i, "fluctuation"] = "right_sell"
+                    last_index = i+1
+                    break
+                elif df.iloc[j]["high"] - df.iloc[i]["low"] > 1.0:
+                    print("sell")
+                    print(df.iloc[j]["time_str"])
+                    print(df.iloc[j]["high"])
+                    print("buy")
+                    print(df.iloc[i]["time_str"])
+                    print(df.iloc[i]["low"])
+                    print("----")
+                    #df.iloc[j]["fluctuation"] = "sell"
+                    #df.iloc[i]["fluctuation"] = "buy"
+                    df.loc[j, "fluctuation"] = "left_sell"
+                    df.loc[i, "fluctuation"] = "right_buy"
+                    last_index = i+1
+                    break
+        return df
+
+if __name__ == "__main__":
+    code = "TSLA"
+    date = "2024-03-21"
+    data = pd.read_csv('/root/program_trading/data/tiger_1m_log_after/{}/{}/{}.csv'.format(code, date[:7], date))
+    data = FeatureBuilder().add_fluctuation(data)
+    print(data)
